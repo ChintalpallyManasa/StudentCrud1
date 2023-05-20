e0dcd43f1b8c86fd1742e797f57a3e624c2edf00
unknown <chintalpallymanasa2gmail.com>
2023-05-20 12:45:29 +0530

    student first crud operatios

---
+++ src/Components/edit-student.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+class EditStudent extends React.Component{
+    render(){
+    return(
+        <div>
+            <h1>edit Student</h1>
+        </div>
+    )
+    }
+}
+export default EditStudent;
